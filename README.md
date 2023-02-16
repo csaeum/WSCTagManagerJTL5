3d87c5ddb9d4bf7b4092d6090fb18b848ab0cb9f
Christian Säum | WSC <github@web-seo-consulting.eu>
2023-02-16 18:18:47 +0100

    Beschreibung
    Signed-off-by: Christian Säum | Linux <github@web-seo-consulting.eu>

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # WSCTagManagerJTL5
+
 TagManager für JTL Shop 5
+
+## Die Verwendung des Plugins erfolgt auf eigene Gefahr ohne jegliche Haftungsansprüche!
+
+## Hinweise zum Plugin und dessen Anfängen im JTL-Forum!
+
+Dieses Plugin in seiner ersten Phase wurde vom
+JTL-Forums-User <a href="https://forum.jtl-software.de/members/qwartz.39285" target="_blank">Qwartz</a> erstellt.
+Der Beitrag dazu
+ist <a href="https://forum.jtl-software.de/threads/jtl5-plugin-google-analytics-tagmanager.145238" target="_blank">
+hier</a> zu finden.
+Er stellte das Plugin dort zum kostenlosen Download zur Verfügung und gibt Support.
+Neuere Versionen seiner Plugins sind auch
+auf <a href="https://www.nicson.de/Google-Analytics-TagManager-fuer-JTL-5" target="_blank">seiner Webseite</a> zu
+finden.</p>
+
+<h2 class="h3">Die Anfänge dieses Plugins auf Basis des Plugins von Nicson</h2>
+<p>Nach Rücksprache mit Nicson habe ich die Erlaubnis von ihm erhalten auf Basis seines Plugins, dieses zu erweitern.
+Dieses Plugin ist unter der <a href="https://github.com/csaeum/WSCTagManagerJTL5" target="_blank">GitHub Adresse</a> erhältlich.</p>
--- adminmenu/templates/hinweise.php
+++ adminmenu/templates/hinweise.php
@@ -1,19 +1,24 @@
 <h1>Die Verwendung des Plugins erfolgt auf eigene Gefahr ohne jegliche Haftungsansprüche!</h1>
-<h1 class="h2">Hinweise zum Plugin und dessen Anfängen im JTL-Forum!</h1>
-<p>Dieses Plugin in seiner ersten Phase wurde vom JTL-Forums-User <a href="https://forum.jtl-software.de/members/qwartz.39285" target="_blank">Qwartz</a> erstellt.
-Der Beitrag dazu ist <a href="https://forum.jtl-software.de/threads/jtl5-plugin-google-analytics-tagmanager.145238" target="_blank">hier</a> zu finden.
-Er stellte das Plugin dort zum kostenlosen Download zur Verfügung und gibt Support.
-Neuere Versionen seiner Plugins sind auch auf <a href="https://www.nicson.de/Google-Analytics-TagManager-fuer-JTL-5" target="_blank">seiner Webseite</a> zu finden.</p>
+<h1>Hinweise zum Plugin und dessen Anfängen im JTL-Forum!</h1>
+<p>Dieses Plugin in seiner ersten Phase wurde vom JTL-Forums-User <a
+            href="https://forum.jtl-software.de/members/qwartz.39285" target="_blank">Qwartz</a> erstellt.
+    Der Beitrag dazu ist <a href="https://forum.jtl-software.de/threads/jtl5-plugin-google-analytics-tagmanager.145238"
+                            target="_blank">hier</a> zu finden.
+    Er stellte das Plugin dort zum kostenlosen Download zur Verfügung und gibt Support.
+    Neuere Versionen seiner Plugins sind auch auf <a href="https://www.nicson.de/Google-Analytics-TagManager-fuer-JTL-5"
+                                                     target="_blank">seiner Webseite</a> zu finden.</p>
 
 <h2 class="h3">Die Anfänge dieses Plugins auf Basis des Plugins von Nicson</h2>
 <p>Nach Rücksprache mit Nicson habe ich die Erlaubnis von ihm erhalten auf Basis seines Plugins, dieses zu erweitern.
-Dieses Plugin ist unter der <a href="https://github.com/csaeum/WSC-Nicson-TM" target="_blank">GitHub Adresse</a> erhältlich.</p>
+    Dieses Plugin ist unter der <a href="https://github.com/csaeum/WSCTagManagerJTL5" target="_blank">GitHub Adresse</a>
+    erhältlich.</p>
 
 <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
 </div>
